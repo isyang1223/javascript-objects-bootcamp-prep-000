7842c396a9d2f0d4ad6366f6e430b4147596b77c
isyang1223 <isyang1223@gmail.com>
2017-09-26 20:02:11 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,11 +1,11 @@
 var playlist = {Usher: 'My boo',
                 'Michale Jackson': 'Thriller'}
-function updatePlaylist(playlist, artist, song) {
-  playlist[artist] = song;
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
   return playlist;
 }
 
-function removeFromPlaylist(playlist, artist) {
-  delete playlist[artist];
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
   return playlist;
 }
